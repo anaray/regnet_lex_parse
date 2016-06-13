025acad3f7ffd9e5afedf1811627a1309f8af471
Anumodh Narayanankutty <anumodh.n@metrticstream.com>
2016-06-13 11:38:38 -0700

    initial commit for lexer

--- token.go
+++ token.go
@@ -1,9 +1,9 @@
 package regnet_lex_parse
 
-type tokenType int
+type TokenType int
 
 const (
-	IILEGAL tokenType = iota
+	IILEGAL TokenType = iota
 	KEY
 	EQUALS
 	PERCENT
@@ -11,6 +11,20 @@ const (
 	END_BRACE
 	REG_TEXT
 	WHITE_SPACE
+	EOF
+	SOME_TEXT_UNMARKED
 )
 
 const eof = rune(0)
+
+type Position int
+
+type Token struct {
+  Type TokenType
+	Pos Position
+  Text string
+}
+
+func (t Token) String() string {
+		return t.Text
+}
