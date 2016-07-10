a2648e20bc2fed0aabd25fee019b12c3c4cd316a
anumodh <anumodh@gmail.com>
2016-07-09 17:35:26 -0700

    adding tests

--- scanner_test.go
+++ scanner_test.go
@@ -10,4 +10,8 @@ var s *Scanner
 
 func TestScanner(t *testing.T) {
 	s = NewScanner(strings.NewReader("NAME=%{NAME_DEF}"))
+	token := s.Scan()
+  if token != "NAME" {
+		t.Error("expected token NAME")
+	}
 }
