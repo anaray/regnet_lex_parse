1fdb85f05b7da9c10c64cb03c0d6b864012587d6
anumodh <anumodh@gmail.com>
2016-07-09 23:24:59 -0700

    adding tests

--- scanner_test.go
+++ scanner_test.go
@@ -11,7 +11,27 @@ var s *Scanner
 func TestScanner(t *testing.T) {
 	s = NewScanner(strings.NewReader("NAME=%{NAME_DEF}"))
 	token := s.Scan()
-	if token != "NAME" {
-		t.Error("expected token NAME")
+	if token.Type != KEY {
+		t.Error("expected token type KEY")
+	}
+
+	if token.Text != "NAME" {
+		t.Error("expected token text NAME")
+	}
+
+	token = s.Scan()
+	if token.Type != EQUALS {
+		t.Error("expected token type EQUALS")
+	}
+
+	if token.Text != "=" {
+		t.Error("expected token text EQUALS")
+	}
+
+	s = NewScanner(strings.NewReader(" "))
+	token = s.Scan()
+
+	if token.Type != EOF {
+		t.Error("expected token type EOF")
 	}
 }
--- token.go
+++ token.go
@@ -28,3 +28,28 @@ type Token struct {
 func (t Token) String() string {
 	return t.Text
 }
+
+func (typ TokenType) String() string {
+	switch typ {
+	case IILEGAL:
+		return "IILEGAL"
+	case KEY:
+		return "KEY"
+	case EQUALS:
+		return "EQUALS"
+	case PERCENT:
+		return "PERCENT"
+	case START_BRACE:
+		return "START_BRACE"
+	case END_BRACE:
+		return "END_BRACE"
+	case REG_TEXT:
+		return "REG_TEXT"
+	case WHITE_SPACE:
+		return "WHITE_SPACE"
+	case EOF:
+		return "EOF"
+	default:
+		panic("unrecognized token")
+	}
+}
